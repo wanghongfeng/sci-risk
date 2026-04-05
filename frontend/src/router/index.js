a5835ef00bbc78b067530c23077fee5c97c93749
LENOVO\wanghf6 <wanghf6@lenovo.com>
2026-04-05 21:44:59 +0800

    fix: 添加 createWebHashHistory import 缺失

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Layout from '../views/Layout.vue'
 import Dashboard from '../views/Dashboard.vue'
 import MenuManage from '../views/MenuManage.vue'
